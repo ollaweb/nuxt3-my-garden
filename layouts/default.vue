bef305b67d093af1b9a7fe3482fb3ddc9308b0b6
Olga L <huzijm@yandex.ru>
2023-04-17 15:28:39 +0300

    feature: hid auth buttons

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,8 +10,8 @@
             class="text-green-600 font-bold hover:text-green-700 transition-colors text-xl"
             >My Garden App</NuxtLink
           >
-          <ul class="flex gap-2 items-center">
-            <li>
+          <ul class="flex gap-8 items-center">
+            <!-- <li>
               <button
                 type="button"
                 class="text-white bg-green-600 hover:bg-green-700 font-medium rounded-lg text-sm px-5 py-2.5"
@@ -26,6 +26,20 @@
               >
                 <NuxtLink to="/register">Register</NuxtLink>
               </button>
+            </li> -->
+            <li>
+              <NuxtLink
+                to="/"
+                class="font-medium hover:text-green-600 transition-colors"
+                >Home</NuxtLink
+              >
+            </li>
+            <li>
+              <NuxtLink
+                to="/about"
+                class="font-medium hover:text-green-600 transition-colors"
+                >About</NuxtLink
+              >
             </li>
           </ul>
         </nav>
---
+++ pages/users/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute();
+</script>
+
+<template>
+  <div class="w-full h-full mx-auto max-w-screen-xl px-4 py-8">
+    <h2 class="text-xl text-green-600">{{ route.params.id }}</h2>
+    <p>Welcome to your homepage!</p>
+  </div>
+</template>
