a3afc18ead2e65d0e0520ed283118dbf49c623f2
Olga L <huzijm@yandex.ru>
2023-04-19 08:40:35 +0300

    feature: updated about page with rules

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="w-full h-full mx-auto max-w-screen-xl px-4 py-8">
-    <h2 class="text-xl text-green-600">My Garden App</h2>
+  <div class="w-full h-full mx-auto max-w-screen-xl px-4 py-8 text-gray-700">
+    <h2 class="text-xl font-bold text-green-600">My Garden App</h2>
     <p>This is a small app, written on Vue.js 3 (Nuxt.js 3).</p>
     <h3 class="mt-2 text-green-600">What does this app can do?</h3>
     <p>In this app you can grow your small garden.</p>
+    <h2 class="mt-4 text-xl font-bold text-green-600">Rules</h2>
+    <ul class="space-y-1 list-disc list-inside dark:text-gray-400">
+      <li>Click the button 'Start now' and enter your name to continue;</li>
+      <li>Seed some plants and wait them to grow, sell your harvest;</li>
+    </ul>
   </div>
 </template>
