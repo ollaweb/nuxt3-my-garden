863bc54f8226ffdaf42e1dd7336f232d7307a32b
Olga L <huzijm@yandex.ru>
2023-04-19 09:08:05 +0300

    feature: added Inter font to the project

--- components/ProfileControls.vue
+++ components/ProfileControls.vue
@@ -1,24 +1,30 @@
 <script setup lang="ts">
-import { usePlayerStore } from "@/stores/PlayerStore";
-const playerStore = usePlayerStore();
+import { usePlayerStore } from '@/stores/PlayerStore'
+const playerStore = usePlayerStore()
 
 let progress =
   (playerStore.player.levelPoints * 100) /
-  playerStore.player.pointsToTheNextLevel;
+  playerStore.player.pointsToTheNextLevel
 
-let degree = (360 / 100) * progress;
+let degree = (360 / 100) * progress
 
-let stringDegreeToCSS = degree.toString() + "deg";
+let stringDegreeToCSS = degree.toString() + 'deg'
 </script>
 
 <template>
   <div v-show="playerStore.player.name">
     <div class="flex justify-between items-center">
-      <h2>{{ playerStore.player.name }}'s garden</h2>
-      <div
-        class="relative level-progress w-8 h-8 rounded-full flex items-center justify-center before:content-[''] before:w-6 before:h-6 before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-fuchsia-300"
-      >
-        <span class="z-[2]">{{ playerStore.player.level }}</span>
+      <h2 class="font-medium">
+        <span>{{ playerStore.player.name }}</span
+        >'s garden
+      </h2>
+      <div class="flex items-center gap-2">
+        level
+        <div
+          class="relative level-progress w-8 h-8 rounded-full flex items-center justify-center before:content-[''] before:w-6 before:h-6 before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-fuchsia-300"
+        >
+          <span class="z-[2]">{{ playerStore.player.level }}</span>
+        </div>
       </div>
     </div>
   </div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
-  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@pinia/nuxt"],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@pinia/nuxt'],
   ssr: false,
-};
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'
+        }
+      ]
+    }
+  },
+  css: ['~/assets/css/main.css']
+}
