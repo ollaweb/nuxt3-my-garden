22a57190296d6034d8de27317543a050567bb883
Olga L <huzijm@yandex.ru>
2023-04-24 09:04:13 +0300

    feature: added tabs to switch between stock seeds and store seeds

---
+++ components/ProfileSeeds.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import SeedsList from './UI/SeedsList.vue';
+
+import { usePlayerStore } from '@/stores/PlayerStore';
+import { usePlantsStore } from '@/stores/PlantsStore';
+
+const playerStore = usePlayerStore();
+const plantsStore = usePlantsStore();
+
+const tab = ref('stock');
+</script>
+
+<template>
+  <ul
+    class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400"
+  >
+    <li
+      @click="tab = 'stock'"
+      class="mr-2 inline-block p-4 text-green-600 rounded-t-lg cursor-pointer"
+      :class="{ 'bg-gray-100': tab == 'stock' }"
+    >
+      Stock
+    </li>
+    <li
+      @click="tab = 'store'"
+      class="mr-2 inline-block p-4 text-green-600 rounded-t-lg cursor-pointer"
+      :class="{ 'bg-gray-100': tab == 'store' }"
+    >
+      Store
+    </li>
+  </ul>
+  <SeedsList v-show="tab == 'stock'" :store="playerStore.player.stock" />
+  <SeedsList v-show="tab == 'store'" :store="plantsStore.plants.store" />
+</template>
---
+++ components/UI/SeedsList.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SeedCard from '@/components/UI/SeedCard.vue';
+
+const props = defineProps({
+  store: {
+    type: Object,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="pb-2">
+    <div class="mt-4" v-for="plantType in props.store" :key="plantType.id">
+      <i class="text-gray-500">{{ plantType.name }}</i>
+      <div class="mt-1 grid grid-cols-3 gap-4">
+        <SeedCard
+          v-for="item in plantType.items"
+          :item="item"
+          :key="item.id"
+          :plant-type-id="plantType.id"
+        />
+      </div>
+    </div>
+  </div>
+</template>
