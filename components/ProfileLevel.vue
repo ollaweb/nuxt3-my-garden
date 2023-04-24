cd64de06c1d8fc0ce98947c6b9143f1223e1d8ab
Olga L <huzijm@yandex.ru>
2023-04-24 09:03:29 +0300

    refactor: separated info block into components

---
+++ components/ProfileLevel.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { usePlayerStore } from '@/stores/PlayerStore';
+const playerStore = usePlayerStore();
+
+let progress = computed(() => {
+  return (
+    (
+      ((360 / 100) * (playerStore.player.level.levelPoints * 100)) /
+      playerStore.player.level.pointsToTheNextLevel
+    ).toString() + 'deg'
+  );
+});
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    level
+    <div
+      class="relative level-progress w-8 h-8 rounded-full flex items-center justify-center before:content-[''] before:w-6 before:h-6 before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:rounded-full before:bg-fuchsia-200"
+    >
+      <span class="z-[2]">{{ playerStore.player.level.number }}</span>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.level-progress {
+  background: conic-gradient(#c026d3 v-bind(progress), #ffffff 0deg);
+}
+</style>
